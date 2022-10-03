fab43017798105e451d478498ba8b7d8f4b13110
syh <syh20131292@gmail.com>
2022-10-04 07:25:07 +0900

    221001 syh
    jointree cctv 학습 코드 추가

---
+++ src/.ipynb_checkpoints/track-checkpoint.sh
@@ -0,0 +1,14 @@
+#python track.py mot --val_mot16 True --load_model ../models/fairmot_yolov5s.pth --image-width 288 --image-height 160 --conf_thres 0.4 --arch yolo --reid_dim 64 --gpus 1 --seqs 1 --run-rknn ../models/yolov5.rknn
+
+#python track.py mot --val_mot16 True --load_model ../models/fairmot_yolov5s.pth --image-width 288 --image-height 160 --conf_thres 0.4 --arch yolo --reid_dim 64 --gpus 1 --seqs 0 --run-rknn ../models/yolov5.rknn
+
+#python track.py mot --val_mot16 True --load_model ../models/fairmot_yolov5s.pth --image-width 1088 --image-height 608 --conf_thres 0.4 --arch yolo --reid_dim 64 --gpus 1 --seqs 0 --run-rknn ../models/yolov5.rknn
+
+#python track.py mot --val_mot16 True --load_model ../models/fairmot_yolov5s.pth --image-width 576 --image-height 320 --conf_thres 0.4 --arch yolo --reid_dim 64 --gpus 1 --seqs 0 --run-rknn ../models/yolov5.rknn
+
+#python track.py mot --val_mot16 True --load_model ../models/fairmot_yolov5s.pth --image-width 864 --image-height 480 --conf_thres 0.4 --arch yolo --reid_dim 64 --gpus 1 --seqs 0 --run-rknn ../models/yolov5.rknn
+
+
+#python track.py mot --val_mot16 True --load_model ../models/model_200.pth --image-width 288 --image-height 160 --conf_thres 0.4 --arch yolo --reid_dim 64 --gpus 1 --seqs 0 #--run-rknn ../models/yolov5.rknn
+
+python track.py mot_att --val_mot16 True --load_model ../models/model_200.pth --image-width 288 --image-height 160 --conf_thres 0.4 --arch yolo --reid_dim 64 --gpus 1 --seqs 0 #--run-rknn ../models/yolov5.rknn
