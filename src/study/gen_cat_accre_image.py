ec55e4dc8d0efb3fb502ab635ed5ac972fa03dcf
syh <syh20131292@gmail.com>
2023-09-14 19:45:21 +0900

    update : 데이터 셋업 코드 추가 [23.09.14]
    3차 정량목표치 달성을 위한 데이터 셋업 코드 추가
    
    src/study/3rd_accred_data_ready.ipynb

---
+++ src/study/gen_cat_accre_image.py
@@ -0,0 +1,30 @@
+'''
+by syh
+
+공인시험 성적을 위한 30인 이상 데이터 생성 코드
+
+jointree_220707 (ID 51~)
+jointree_221005 (ID 151~)
+230629_cam20_2519~2618 ( 13:43:10.414~) (ID 1~)
+230727_cam40_0~99 (ID 101~)
+
+directory tree
+
+3rd_dir
+    -20220707_jointree
+    -20221005_jointree
+    -20230629_gjtp
+    -20230727_gjtp
+
+'''
+
+DATA_ROOT= '/media/syh/ssd2/data/accre_3rd_data'
+
+# gt_join_0707
+# gt_join_1005
+# gt_gjtp_0629
+# gt_gjtp_0727
+
+
+
+
